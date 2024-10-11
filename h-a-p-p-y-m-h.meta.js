8a77296f1a515d4b2b713fb56a81c400876550c3
ishidaren3456 <ishidaren3456@github.com>
2024-10-11 10:50:29 +0700

    init

---
+++ h-a-p-p-y-m-h.meta.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         h-a-p-p-y-m-h
+// @namespace    h-a-p-p-y-m-h.user.js
+// @version      2024.10.11
+// @description  down h-a-p-p-y-m-h
+// @author       You
+// @include      /^https:\/\/m\.h[a4]p[p9]ymh\.c[o0]m\/r[e3]ads\//
+// @icon         https://raw.githubusercontent.com/ishidaren3456/tu_xe_trau/main/h-a-p-p-y-m-h.png
+// @grant        GM_xmlhttpRequest
+// @run-at       document-start
+// @require      https://cdn.jsdelivr.net/npm/@zip.js/zip.js@2.7.48/dist/zip.min.js
+// @downloadURL  https://github.com/ishidaren3456/tu_xe_trau/raw/main/h-a-p-p-y-m-h.user.js
+// @updateURL    https://github.com/ishidaren3456/tu_xe_trau/raw/main/h-a-p-p-y-m-h.meta.js
+// ==/UserScript==
